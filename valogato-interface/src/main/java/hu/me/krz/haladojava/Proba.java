d003f3f515d5ab5c6a15ef38f8795fec9ea098f2
Gnadig Peter Levente <Gnadig Peter Levente@ASUS-FX504GD-GP>
2020-02-19 15:39:09 +0100

    5.2

--- valogato-interface/src/main/java/hu/me/krz/haladojava/Proba.java
+++ valogato-interface/src/main/java/hu/me/krz/haladojava/Proba.java
@@ -1,5 +1,6 @@
 package hu.me.krz.haladojava;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Proba {
@@ -17,8 +18,41 @@ public class Proba {
 		}
 		sc.close();
 		
-		Szemely[] szemelyek = new Szemely[db];
-		//for()
+		
+		Diak[] diakok = new Diak[db];
+		Tanar[] tanarok = new Tanar[db];
+
+		for (int i = 0; i < diakok.length; i++) {
+			String name = "diák" + (i + 1);
+			Random r = new Random();
+			int age = r.nextInt(80 - 16 + 1) + 16;
+			int puska = r.nextInt(6 - 0) % 6;
+			diakok[i] = new Diak(name, age, puska);
+		}
+
+		for (int i = 0; i < tanarok.length; i++) {
+			String name = "tanár" + (i + 1);
+			Random r = new Random();
+			int age = r.nextInt(80 - 25 + 1) + 25;
+			double atlag = r.nextDouble() * (5 - 1) + 1;
+			tanarok[i] = new Tanar(name, age, atlag);
+		}
+		
+		for (Diak d : diakok) {
+			if (d.joAlanyE()) {
+				System.out.println(d);
+			}
+		}
+
+		for (Tanar t : tanarok) {
+			if (t.joAlanyE()) {
+				System.out.println(t);
+			}
+		}
+		
+		
+		
+		
 		
 		
 	}
