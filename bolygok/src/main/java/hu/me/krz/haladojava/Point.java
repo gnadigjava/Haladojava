817b8bd790253e5e3b329635bf6ff4f12a8f8710
Gnadig Peter Levente <Gnadig Peter Levente@ASUS-FX504GD-GP>
2020-02-19 13:50:46 +0100

    a

--- bolygok/src/main/java/hu/me/krz/haladojava/Point.java
+++ bolygok/src/main/java/hu/me/krz/haladojava/Point.java
@@ -1,5 +1,32 @@
 package hu.me.krz.haladojava;
 
 public class Point {
-
+	
+	private long x;
+	private long y;
+	private long z;
+	
+	public Point(long x, long y, long z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public long getX() {
+		return x;
+	}
+	public long getY() {
+		return y;
+	}
+	public long getZ() {
+		return z;
+	}
+	
+	public void translate(Point move) {
+		this.x = move.x;
+		this.y = move.y;
+		this.z = move.z;
+	}
+	
 }
