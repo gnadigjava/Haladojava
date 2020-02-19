52bf7b957b935b5decdf04b51f3b0f35a21ec304
Gnadig Peter Levente <Gnadig Peter Levente@ASUS-FX504GD-GP>
2020-02-19 14:19:47 +0100

    1.

---
+++ diak-puska/src/main/java/hu/me/krz/haladojava/Szemely.java
@@ -0,0 +1,21 @@
+package hu.me.krz.haladojava;
+
+public class Szemely {
+	
+	private final String name;
+	private final int age;
+	
+	public Szemely(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+}
