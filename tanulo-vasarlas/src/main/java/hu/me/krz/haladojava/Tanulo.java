e6c6686b82b5507f82a70bdc822d7fc41b06d2ab
Gnadig Peter Levente <Gnadig Peter Levente@ASUS-FX504GD-GP>
2020-02-19 15:44:40 +0100

    1.1

---
+++ tanulo-vasarlas/src/main/java/hu/me/krz/haladojava/Tanulo.java
@@ -0,0 +1,24 @@
+package hu.me.krz.haladojava;
+
+public class Tanulo {
+	private final String nev;
+	private int penz;
+	
+	public Tanulo(String nev, int penz) {
+		super();
+		this.nev = nev;
+		this.penz = penz;
+	}
+	
+	private String getNev() {
+		return nev;
+	}
+
+	private int getPenz() {
+		return penz;
+	}
+
+	
+	
+	
+}
