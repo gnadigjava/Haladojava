41d17a52a0b97786b34aa3a219f1a91b8e13a20d
Gnadig Peter Levente <Gnadig Peter Levente@ASUS-FX504GD-GP>
2020-02-19 14:30:56 +0100

    2

---
+++ diak-puska/src/main/java/hu/me/krz/haladojava/Diak.java
@@ -0,0 +1,29 @@
+package hu.me.krz.haladojava;
+
+public class Diak extends Szemely implements Comparable<Diak> {
+
+	private int db;
+	
+	
+	public Diak(String name, int age, int db) {
+		super(name, age);
+		this.db = db;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Diak [db=" + db + "]";
+	}
+
+
+	@Override
+	public int compareTo(Diak o){
+		if (this.db == o.db)
+			return 0;
+		else if (this.db > o.db)
+			return 1;
+		else
+			return -1;
+	}
+}
