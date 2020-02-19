4c82e1938cef7800ae6fcccbf0d5876ddaf6c933
Gnadig Peter Levente <Gnadig Peter Levente@ASUS-FX504GD-GP>
2020-02-19 16:52:24 +0100

    1.talán2?

--- tanulo-vasarlas/src/main/java/hu/me/krz/haladojava/Tanulo.java
+++ tanulo-vasarlas/src/main/java/hu/me/krz/haladojava/Tanulo.java
@@ -3,11 +3,17 @@ package hu.me.krz.haladojava;
 public class Tanulo {
 	private final String nev;
 	private int penz;
+	private int tanulok=0;
 	
 	public Tanulo(String nev, int penz) {
 		super();
 		this.nev = nev;
 		this.penz = penz;
+		tanulok++;
+	}
+	
+	public Tanulo(String nev) {
+		this(nev, 10000);
 	}
 	
 	private String getNev() {
@@ -18,7 +24,28 @@ public class Tanulo {
 		return penz;
 	}
 
+	public int getOssztanulo() {
+		return tanulok;
+	}
 	
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		tanulok--;
+	}
+
+	@Override
+	public String toString() {
+		return "Tanulo [nev=" + nev + "]" + ", penz=" + penz;
+	}
 	
+	public boolean fogyasztas(int koltseg) {
+		this.penz-=koltseg;
+		if(this.penz < 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 	
 }
