82db9196f46e40ffc5638ed9924f5a00d9d4505f
gnadigjava <61229611+gnadigjava@users.noreply.github.com>
2020-04-22 19:16:22 +0200

    fos

---
+++ calculator-logic1/src/test/java/calculatorLogic/LogicTest.java
@@ -0,0 +1,50 @@
+package calculatorLogic;
+import static org.junit.Assert.assertEquals;
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+import calculatorLogic.Logic;
+
+class LogicTest {
+    Logic l = new Logic();
+
+    @Test
+    void osszeadas() {
+        //Mock
+        //Logic l = mock(Logic.class);
+
+        //When
+        //when(l.getA()).thenReturn(5.0);
+        //when(l.getB()).thenReturn(6.0);
+
+        //Assert
+        assertEquals((l.osszeadas(5, 6)), 11.0);
+    }
+
+    @Test
+    void kivonas() {
+        assertEquals((l.kivonas(5, 6)), -1.0);
+    }
+
+    @Test
+    void szorzas() {
+        assertEquals((l.szorzas(5, 6)), 30);
+    }
+
+    @Test
+    void szorzasNullaval() {
+        assertEquals((l.szorzas(5, 0)), 0);
+    }
+
+    @Test
+    void osztas() {
+        assertEquals((l.osztas(10, 0)), Double.POSITIVE_INFINITY);
+    }
+
+    @Test
+    void osztasNullavalNegativat() {
+        assertEquals((l.osztas(-10, 0)), Double.NEGATIVE_INFINITY);
+    }
+}
---
+++ calculator-logic1/src/test/java/calculatorLogic/LogicTeszt.java
@@ -0,0 +1,19 @@
+package calculatorLogic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+class LogicTeszt {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
